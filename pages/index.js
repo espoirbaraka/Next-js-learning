963f07bc71dd7fc37355dc93d4d59c0736a94a45
espoirbaraka <esbarakabigeba@gmail.com>
2022-06-28 13:25:57 +0200

    Components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import appbar from "../components/appbar";
+import ResponsiveAppBar from "../components/appbar";
 
 export default function Home() {
   return (
       <>
+          <ResponsiveAppBar/>
           <span>Bonjour</span>
       </>
   )
